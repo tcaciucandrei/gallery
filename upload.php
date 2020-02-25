d6c0c19e35f9057b2c2c27b997b7e180be330bc9
tcaciucandrei <46537455+tcaciucandrei@users.noreply.github.com>
2020-02-25 06:40:00 +0200

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,103 @@
+<?php include("includes/header.php"); ?>
+
+<?php if(!$session->is_signed_in()) { header("Location: login.php"); } ?>
+         
+         
+<?php
+
+
+if(isset($_POST['submit'])){
+    
+    $photo = new Photo();
+   
+    $photo -> title = $_POST['title'];
+    
+    $photo -> set_file($_FILES['file_upload']);
+    
+    
+    
+}
+
+?>
+         
+         
+        
+         
+         
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+   <?php include("includes/top_nav.php") ?>
+
+
+    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+
+
+    <?php include("includes/side_nav.php")?>
+
+    <!-- /.navbar-collapse -->
+</nav>
+
+<div id="page-wrapper">
+
+    <div class="container-fluid">
+
+       <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                   UPLOAD
+                    <small>Subheading</small>
+                </h1>
+
+
+               <div class="col-md-6">
+                <form action="upload.php" method="post" enctype="multipart/form-data">
+
+                    <div class="form-group">
+
+                        <input type="text" name="title" class="form-control">
+
+                    </div>
+
+                     <div class="form-group">
+
+                        <input type="file" name="file_upload">
+
+                    </div>
+
+                    <input type="submit" name="submit">
+
+                </form>
+
+                </div>
+
+
+
+
+
+
+
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container-fluid -->
+
+
+
+    
+    
+    
+    
+
+</div>
+<!-- /#page-wrapper -->
+
+
+
+
+
+            
+            <?php include("includes/footer.php"); ?>
